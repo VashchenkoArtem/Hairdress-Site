a9438eb07564986d5f82c38c959c982c66dfe223
Artem <artemvaschenko83@gmail.com>
2025-10-29 12:58:39 +0200

    Creating modal form for phone

--- haircut/main/static/main/js/main.js
+++ haircut/main/static/main/js/main.js
@@ -1,9 +1,13 @@
 const buttonMenu = document.querySelector(".button-menu");
+const buttonMenuPhone = document.querySelector(".button-menu-phone");
 const menuContainer = document.querySelector(".modal-hat-frame");
 const closeMenu = document.querySelector(".button-cross");
+const closeMenuPhone = document.querySelector(".button-cross-phone");
 const screenWidth = window.innerWidth;
 const laptopDesign = document.querySelector(".design-for-laptop");
 const phoneDesign = document.querySelector(".design-for-phone");
+const modalPhone = document.querySelector(".modal-frame-phone");
+const titlePhone = document.querySelector(".title-hat-phone");
 
 
 if (screenWidth < 768){
@@ -26,3 +30,14 @@ closeMenu.addEventListener("click", ()=> {
     menuContainer.classList.toggle("hide-or-show-modal");
 })
 
+buttonMenuPhone.addEventListener("click", () => {
+    buttonMenuPhone.classList.toggle("hide");
+    titlePhone.classList.toggle("hide");
+    modalPhone.classList.toggle("hide");
+})
+
+closeMenuPhone.addEventListener("click", () => {
+    buttonMenuPhone.classList.toggle("hide");
+    titlePhone.classList.toggle("hide");
+    modalPhone.classList.toggle("hide");
+})
